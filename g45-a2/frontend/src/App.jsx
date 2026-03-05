c1313c152736fe9fd1a96b0b620e4d0e1848cfed
Mo Nakouzi <mohamad.nakouzi@dyedurham.com>
2026-03-05 13:13:13 -0500

    Adding skeleton for a2 code

---
+++ g45-a2/frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="bg-red-500 min-h-screen min-w-[50vw] flex items-center justify-center text-white">
+            test
+        </div>
+    )
+}
